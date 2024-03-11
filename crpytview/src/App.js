3e340999ddad5c9de87af20c371b7982a6e523e5
Wildskiller <albert_beaupre@hotmail.com>
2024-03-10 19:32:33 -0500

    API/Secret Key and Exchange Fetch
    I added an example of how to retrieve the JSON exchange information based on the provided symbol.
    
    The queryString and signature constants are used for obtaining information about the status, time, and other trading information.

--- crpytview/src/App.js
+++ crpytview/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Chart } from "react-google-charts";
+import CryptoJS from 'crypto-js';
 
-function App() {
+
+export function App() {
+  const secretKey = 'aPWOT1QqFA7v6QqH4h6DdtseVX0Dl5YZ1sBK7LCVfGoArFmt5Ntumo3l7DMtQARo' // We gotta store this properly somewhere
+  const apiKey = 'uVrzyFyZLzRHnnvm2jeQtW2jv4fEh8cLH1qlaCk5BKVr1lR4KnUVJY6Bv4CQKClB'; // We gotta store this properly somewhere as well
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => { fetchBinanceData('BTCUSDT'); }, []);
+
+  const fetchBinanceData = async (symbol) => {
+    const timestamp = Date.now();
+    const queryString = `timestamp=${timestamp}`;
+    const signature = CryptoJS.HmacSHA256(queryString, secretKey).toString(CryptoJS.enc.Hex);
+    const apiUrl = `https://api.binance.us/api/v3/exchangeInfo?symbol=${symbol}`;
+
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const data = await response.json();
+    setData(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
