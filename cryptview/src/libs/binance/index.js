ac4b56fbb279aa59229c7091e17a6a395cf5650c
TonyEra <tonyera789@gmail.com>
2024-03-28 19:35:51 -0400

    Code organization

--- cryptview/src/libs/binance/index.js
+++ cryptview/src/libs/binance/index.js
@@ -1,12 +1,6 @@
 import CryptoJS from 'crypto-js';
 
-/**
- * This is the API host for binance.
- */
-export const HOST = 'https://api.binance.us';
-export const API_KEY = 'uVrzyFyZLzRHnnvm2jeQtW2jv4fEh8cLH1qlaCk5BKVr1lR4KnUVJY6Bv4CQKClB';
-export const SECRET_KEY = 'aPWOT1QqFA7v6QqH4h6DdtseVX0Dl5YZ1sBK7LCVfGoArFmt5Ntumo3l7DMtQARo';
-
+const SECRET_KEY = process.env.REACT_APP_BINANCE_SECRET_KEY;
 /**
  * This is the only available interval values from here: https://docs.binance.us/?shell#trade-data-stream
  */
@@ -26,65 +20,6 @@ export const CandlestickIntervals = {
     THREE_DAYS: '3d',
     ONE_WEEK: '1w',
     ONE_MONTH: '1M'
-};
-
-/**
- * These are the types of requests that can be made to the Binance API.
- */
-export const Requests = {
-    /**
-     * @param {*} parameters[0] The cryptocurrency symbol
-     * @param {*} parameters[1] The interval span that the sticks are retrieved from
-     * @param {*} parameters[2] The amount of candlesticks to retrieve
-     * @returns The GET request value for CandleStick data.
-     */
-    CANDLESTICK_DATA: { GET: (parameters) => `/api/v3/klines?symbol=${parameters[0]}&interval=${parameters[1]}&limit=${parameters[2]}` },
-    RECENT_TRADES: { GET: '/api/v3/trades' },
-    PRICE_DATA: { GET: '/api/v3/ticker/price' },
-    AVERAGE_PRICE: { GET: '/api/v3/avgPrice' },
-    EXCHANGE_INFO: { GET: (parameters) => `/api/v3/exchangeInfo?symbols=[${parameters.map(param => `"${param}"`).join(',')}]` },
-
-    /**
-     * This gets the exchange server time.
-     */
-    SERVER_TIME: { GET: '/api/v3/time' },
-
-    /**
-     * This tests the connectivity to the exchange and requires no parameters.
-     */
-    PING: { GET: '/api/v3/ping' },
-
-    /**
-     * This gets the status of the exchange system, whether it is normal or under maintenance.
-     */
-    STATUS: {
-        HEADER: `X-MBX-APIKEY:${API_KEY}`,
-        GET: `/sapi/v1/system/status?timestamp=${Date.now()}&signature=${generateSignature()}`
-    },
-}
-
-/**
- * This function asynchronously requests the specified {@param requestType} from the {@constant HOST}
- * and returns 
- * 
- * @param {*} requestType The type of GET request to get from the Binance host
- * @param  {...any} parameters The parameters (if required) to implement into the GET request
- * @returns The JSON formatted response to the request
- */
-export const fetchBinanceData = async (requestType, ...parameters) => {
-    const endpoint = typeof requestType.GET === 'function' ? requestType.GET(parameters) : requestType.GET;
-    const headers = requestType.HEADER ? { 'HEADER': requestType.HEADER } : {};
-    const request = `${HOST}${endpoint}`;
-    const response = await fetch(request, {
-        method: 'GET',
-        headers: headers,
-        'Content-Type': 'application/json',
-    });
-
-    if (!response.ok)
-        throw new Error(`${response.status}`);
-
-    return await response.json();
 };
 
 /**
