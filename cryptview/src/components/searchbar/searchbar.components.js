cda61a6c6678b0ce7c5a7ec7e7317612a6bdb430
TonyEra <tonyera789@gmail.com>
2024-04-18 01:16:39 -0400

    searchbar

--- cryptview/src/components/searchbar/searchbar.components.js
+++ cryptview/src/components/searchbar/searchbar.components.js
@@ -1,4 +1,24 @@
-import { Input } from "shadcn/components/ui/input";
+import { useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from 'shadcn/components/ui/command';
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from 'shadcn/components/ui/popover';
+
 
 /**
  * SearchBar Component is used to query results
@@ -6,22 +26,73 @@ import { Input } from "shadcn/components/ui/input";
  */
 export default function SearchBar(props) {
 
-  let suggestions;
-  //Do we have a search file preloaded associated with the searchbar component
+  const symbolsRef = useRef([]);
+  const [open, setOpen] = useState(false);
+  const {ticker} = useParams();
+
   if(props.suggestions) {
-    suggestions = props.suggestions;
+    //setSymbols(props.suggestions);
+    symbolsRef.current = props.suggestions;
+    //console.log(symbolsRef.current);
   }
 
-  if(suggestions) {
-    $.getJSON(suggestions).success(function (data) {
-      var cryptoArray = data;
-      
-      //auto complete code here
-      console.log('success!');
-    })
-    .error(function() {
-      console.error('Cannot load search file');
-    })
+  const findSymbolMatches = (wordToMatch) => {
+    const symbolsArray = symbolsRef.current;
+
+    return symbolsArray.filter(function(crypto) {
+      return crypto.name.toLowerCase().includes(wordToMatch.toLowerCase()) || 
+      crypto.symbol.toLowerCase().includes(wordToMatch.toLowerCase());
+    });  
+  };
+
+  /**
+   * This should run when the form is submitted ex: enter is pressed on the search bar
+   * @param {event} e 
+   */
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Submitted!');
+  }
+  
+  function handleOnChange(e) {
+    //console.log('changing: ' + e.target.value);
+    let current = e.target.value;
+    console.log(findSymbolMatches(current));
   }
 
+  function handleOnClickItem(value) {
+    console.log('clicked!');
+  }
+  
+  const coinImageStyle = {
+    display: "block",
+    maxWidth: "30px",
+    maxHeight: "30px",
+    paddingInline: "0px 10px"
+  }
+
+  return(
+    <form onSubmit={handleSubmit} onFocus={() => setOpen(true)} onBlur={() => setOpen(false)}>
+        <Command>
+            <CommandInput type="symbol" placeholder="BTC" />
+            {open &&
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup heading="Symbols">
+                {
+                  symbolsRef.current.slice(0,5).map(function(element, i) {
+                    return <CommandItem obj={element} key={i} value={element.symbol} onSelect={handleOnClickItem}><img src={element.icon} style={coinImageStyle} alt={element.name}/><span>{element.symbol}</span></CommandItem>
+                  })
+                }
+              </CommandGroup>
+            </CommandList>
+            }
+          </Command>
+
+
+
+
+
+    </form>
+  )
 }
